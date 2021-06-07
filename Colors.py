769b9812f66dbcde4f6175fde715cf45dcfa71e8
lkashur <59710924+lkashur@users.noreply.github.com>
2021-06-07 08:49:50 -0600

    Add files via upload

---
+++ Colors.py
@@ -0,0 +1,17 @@
+
+# COLORS CLASS {{
+#
+class Colors:
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    CLEAR = "\033[H\033[J"
+#
+# }}
+
+
